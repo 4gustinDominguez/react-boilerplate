50a5a7b36904d6f36de9be3af435ad47dcd7c699
Agustin <l.dominguez.sangla@accenture.com>
2020-02-07 08:32:30 +0100

    hello

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>React Boilrplate!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
